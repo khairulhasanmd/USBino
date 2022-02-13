0808401559ec772c3388ceb336c7f30620e9cd69
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2022-02-13 23:02:35 +0600

    Pin definition file updated for atmega32a

--- avr/variants/sanguino/pins_arduino.h
+++ avr/variants/sanguino/pins_arduino.h
@@ -1,31 +1,26 @@
 /*
   pins_arduino.h - Pin definition functions for Arduino
   Part of Arduino - http://www.arduino.cc/
-
-  Copyright (c) 2007 David A. Mellis
-
+  Copyright (c) 2007 David A. Mellis 
+  Edited By Md. Khairul hasan
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General
   Public License along with this library; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
   Boston, MA  02111-1307  USA
 
-  $Id: wiring.h 249 2007-02-03 16:52:51Z mellis $
-
   Changelog
   -----------
   11/25/11  - ryan@ryanmsutton.com - Add pins for Sanguino 644P and 1284P
   07/15/12  - ryan@ryanmsutton.com - Updated for arduino0101
-  12/24/16  - bob.kuhn@att.net     - add 1284 PWMs
+  12/14/13  - khairulhasanmd@gmail.com - Modified it for supporting Atmega32A
 
   Improvements by Kristian Sloth Lauszus, lauszus@gmail.com
 */
@@ -52,9 +47,9 @@ static const uint8_t SCK  = PIN_SPI_SCK;
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
 
-#define LED_BUILTIN 0
+#define LED_BUILTIN 0 //this pin has changed
 
-#define PIN_A0 (31)
+#define PIN_A0 (31) //analog pins are reversely oriented 
 #define PIN_A1 (30)
 #define PIN_A2 (29)
 #define PIN_A3 (28)
@@ -72,64 +67,68 @@ static const uint8_t A5 = PIN_A5;
 static const uint8_t A6 = PIN_A6;
 static const uint8_t A7 = PIN_A7;
 
-// ATMEL ATMEGA644/ATMEGA1284 / SANGUINO
+// On the Arduino board, digital pins are also used
+// for the analog output (software PWM).  Analog input
+// pins are a separate set.
+
+//                      +-----\/-----+
+//          (D 0) PB0  1|            |40  PA0 (D 31)(AI 0)
+//          (D 1) PB1  2|            |39  PA1 (D 30)(AI 1)
+//     INT2 (D 2) PB2  3|            |38  PA2 (D 29)(AI 2)
+//      PWM (D 3) PB3  4|            |37  PA3 (D 28)(AI 3)
+//          (D 4) PB4  5|            |36  PA4 (D 27)(AI 4)
+//          (D 5) PB5  6|            |35  PA5 (D 26)(AI 5)
+//          (D 6) PB6  7|            |34  PA6 (D 25)(AI 6)
+//          (D 7) PB7  8|            |33  PA7 (D 24)(AI 7)
+//              RESET  9|            |32  AREF
+//                VCC 10| ATMEGA 32A |31  GND
+//                GND 11|            |30  AVCC
+//              XTAL2 12|            |29  PC7 (D 23)
+//              XTAL1 13|            |28  PC6 (D 22)
+//          (D 8) PD0 14|            |27  PC5 (D 21)
+//          (D 9) PD1 15|            |26  PC4 (D 20)
+//    INT0 (D 10) PD2 16|            |25  PC3 (D 19)
+//    INT1 (D 11) PD3 17|            |24  PC2 (D 18)
+//     PWM (D 12) PD4 18|            |23  PC1 (D 17)
+//     PWM (D 13) PD5 19|            |22  PC0 (D 16)
+//         (D 14) PD6 20|            |21  PD7 (D 15) PWM
+//                      +------------+
 //
-//                        +---\/---+
-//            (D 0) PB0  1|        |40  PA0 (AI 0 / D31)
-//            (D 1) PB1  2|        |39  PA1 (AI 1 / D30)
-//       INT2 (D 2) PB2  3|        |38  PA2 (AI 2 / D29)
-//        PWM (D 3) PB3  4|        |37  PA3 (AI 3 / D28)
-//     SS PWM (D 4) PB4  5|        |36  PA4 (AI 4 / D27)
-//       MOSI (D 5) PB5  6|        |35  PA5 (AI 5 / D26)
-//       MISO (D 6) PB6  7|        |34  PA6 (AI 6 / D25)
-//        SCK (D 7) PB7  8|        |33  PA7 (AI 7 / D24)
-//                  RST  9|        |32  AREF
-//                  VCC 10|        |31  GND
-//                  GND 11|        |30  AVCC
-//                XTAL2 12|        |29  PC7 (D 23)
-//                XTAL1 13|        |28  PC6 (D 22)
-//       RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
-//       TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
-//  INT0 RX1 (D 10) PD2 16|        |25  PC3 (D 19) TMS
-//  INT1 TX1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
-//       PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
-//       PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
-//       PWM (D 14) PD6 20|        |21  PD7 (D 15) PWM
-//                        +--------+
-//
+
+
 #define NUM_DIGITAL_PINS            32
 #define NUM_ANALOG_INPUTS           8
+#define analogInputToDigitalPin(p)  ((p < 8) ? 31 - (p): -1) // ((p < 8) ? (p) + 24 : -1) was this
+#define analogPinToChannel(p)       ((p) < 8 ? (p) : (p) >= 24 ? 31 - (p) : -1) //
 
-#define analogInputToDigitalPin(p)  ((p < 8) ? 31 - (p): -1)
-#define analogPinToChannel(p)       ((p) < 8 ? (p) : (p) >= 24 ? 31 - (p) : -1)
 
-#if defined(TCCR3A)
-  #define digitalPinHasPWM(p)       ((p) == 3 || (p) == 4 || (p) == 6 || (p) == 7 || (p) == 12 || (p) == 13 || (p) == 14 || (p) == 15)
-#else
-  #define digitalPinHasPWM(p)       ((p) == 3 || (p) == 4 || (p) == 12 || (p) == 13 || (p) == 14 || (p) == 15)
-#endif
+#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 12 || (p) == 13 || (p) == 15)//done
 
 #define digitalPinToPCICR(p)        ( (((p) >= 0) && ((p) <= 31)) ? (&PCICR) : ((uint8_t *)0) )
+//                                  ( ((p) >= 0 && (p) <= 21) ? (&PCICR) : ((uint8_t *)0))
 
+//                                  ( ((p) <= 7) ? 2 : (((p) <= 13) ? 0 : 1))
 #define digitalPinToPCICRbit(p)     ( (((p) >= 24) && ((p) <= 31)) ? 0 : \
                                     ( (((p) >=  0) && ((p) <=  7)) ? 1 : \
                                     ( (((p) >= 16) && ((p) <= 23)) ? 2 : \
                                     ( (((p) >=  8) && ((p) <= 15)) ? 3 : \
                                     0 ) ) ) )
 
+//                                  (((p) <= 7) ? (&PCMSK2) : (((p) <= 13) ? (&PCMSK0) : (((p) <= 21) ? (&PCMSK1) : ((uint8_t *)0))))
 #define digitalPinToPCMSK(p)        ( (((p) >= 24) && ((p) <= 31)) ? (&PCMSK0) : \
                                     ( (((p) >=  0) && ((p) <=  7)) ? (&PCMSK1) : \
                                     ( (((p) >= 16) && ((p) <= 23)) ? (&PCMSK2) : \
                                     ( (((p) >=  8) && ((p) <= 15)) ? (&PCMSK3) : \
                                     ((uint8_t *)0) ) ) ) )
 
-
+//                                  (((p) <= 7) ? (p) : (((p) <= 13) ? ((p) - 8) : ((p) - 14)))
 #define digitalPinToPCMSKbit(p)     ( (((p) >= 24) && ((p) <= 31)) ? (31 - (p)) : \
                                     ( (((p) >=  0) && ((p) <=  7)) ? (p) : \
                                     ( (((p) >= 16) && ((p) <= 23)) ? ((p) - 16) : \
                                     ( (((p) >=  8) && ((p) <= 15)) ? ((p) - 8) : \
                                     0 ) ) ) )
-
+                                    
+//                                  ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT))
 #define digitalPinToInterrupt(p)    ((p) == 10 ? 0 : ((p) == 11 ? 1 : ((p) == 2 ? 2 : NOT_AN_INTERRUPT)))
 
 #ifdef ARDUINO_MAIN
@@ -237,23 +236,18 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
         NOT_ON_TIMER,   /* 1  - PB1 */
         NOT_ON_TIMER,   /* 2  - PB2 */
         TIMER0A,        /* 3  - PB3 */
-        TIMER0B,        /* 4  - PB4 */
+        NOT_ON_TIMER,   /* 4  - PB4 */
         NOT_ON_TIMER,   /* 5  - PB5 */
-#if defined(TCCR3A)
-        TIMER3A,        /* 6  - PB6 */
-        TIMER3B,        /* 7  - PB7 */
-#else
         NOT_ON_TIMER,   /* 6  - PB6 */
         NOT_ON_TIMER,   /* 7  - PB7 */
-#endif
         NOT_ON_TIMER,   /* 8  - PD0 */
         NOT_ON_TIMER,   /* 9  - PD1 */
         NOT_ON_TIMER,   /* 10 - PD2 */
         NOT_ON_TIMER,   /* 11 - PD3 */
         TIMER1B,        /* 12 - PD4 */
         TIMER1A,        /* 13 - PD5 */
-        TIMER2B,        /* 14 - PD6 */
-        TIMER2A,        /* 15 - PD7 */
+        NOT_ON_TIMER,   /* 14 - PD6 */
+        TIMER2,         /* 15 - PD7 */ //or timer2a?
         NOT_ON_TIMER,   /* 16 - PC0 */
         NOT_ON_TIMER,   /* 17 - PC1 */
         NOT_ON_TIMER,   /* 18 - PC2 */
